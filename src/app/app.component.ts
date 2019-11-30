f4728cb0d9d479e6dbc8e2ac802fe9aa363f7cf7
Arthur Ribeiro <arthurmr13@hotmail.com>
2019-11-30 15:58:02 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private sqlite: SQLite
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.sqlite.create({
+        name: 'aviao',
+        location: 'default'
+      }).then((db: SQLiteObject) => {
+        db.executeSql('create table operacoes(id integer unsigned auto_increment primary key, name varchar(255))');
+      });
+    });
+  }
+}
